e6dce8793d960a73507fb1f6a9575fcf40ae98cc
BZR4 <BZR4@users.noreply.github.com>
2025-08-11 21:42:59 -0300

    Initial Commit

---
+++ src/main/java/br/gov/sp/cps/fatecararas/productms/ProductmsApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.cps.fatecararas.productms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/cps/fatecararas/productms/ProductmsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.cps.fatecararas.productms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
